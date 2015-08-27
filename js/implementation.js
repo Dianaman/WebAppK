e6d6e8a9ac2e4e34f6cb7df95260bc79fa50d87e
Diana <diana.man.91@gmail.com>
2015-08-27 17:17:27 -0300

    modals broken

--- js/implementation.js
+++ js/implementation.js
@@ -37,21 +37,55 @@ function httpGetAsync()
     var usuario;
     var contra;
     xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-        { 
-          document.getElementById("myDiv").innerHTML= xmlHttp.responseText;
+      //jQuery.noConflict();
+      /*if(xmlHttp.readyState >= 0 && xmlHttp.readyState < 4 && xmlHttp.status == 200)
+      {
+        $("#chargeModal").modal();
+        console.log("llego a charge");
+      }*/
+
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+      { 
+        /*console.log("llego a cerrar");
+        //$('#chargeModal').dialog('close');
+        //$("#chargeModal").close();
+        $("#chargeModal").modal('close');*/
+        
+        /*setTimeout(function(){
+          $("#chargeModal").modal('close')
+        }, 300);*/
+
+        //document.getElementById("myDiv").innerHTML= xmlHttp.responseText;
+        if(xmlHttp.responseText=="true")
+        {
+          $("#okModal").modal('show');
+          console.log("llego a ok");
+          /*{
+            document.getElementById("myDiv").innerHTML="Ok";
+          }*/
         }
+        else 
+        {
+          $("#errModal").modal('show');
+          console.log("llego a error");
+          /*{
+            document.getElementById("myDiv").innerHTML="Error";
+          }*/
+        }
+      }
     }
     //xmlHttp.open("POST", "http://localhost/holamundo.php", true); // true for asynchronous 
     //xmlHttp.send(usuario, contra);   
-              usuario = document.getElementById("user").value;
-              contra = document.getElementById("password").value;
-var PageToSendTo = "http://localhost/holamundo.php?";
-var MyVariable = usuario;
-var VariablePlaceholder = "usuario=";
-var MyVariable2 = contra;
-var VariablePlaceholder2 = "contra=";
-var UrlToSend = PageToSendTo + VariablePlaceholder + MyVariable + "&" + VariablePlaceholder2 + MyVariable2;
+ 
+    usuario = document.getElementById("user").value;
+    contra = document.getElementById("password").value;
+
+    var PageToSendTo = "http://localhost/holamundo.php?";
+    var MyVariable = usuario;
+    var VariablePlaceholder = "usuario=";
+    var MyVariable2 = contra;
+    var VariablePlaceholder2 = "contra=";
+    var UrlToSend = PageToSendTo + VariablePlaceholder + MyVariable + "&" + VariablePlaceholder2 + MyVariable2;
 
     xmlHttp.open("post", UrlToSend, true); // true for asynchronous 
     xmlHttp.send(null);
